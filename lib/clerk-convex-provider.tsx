fa12c322b68d2d82569f9df900ae6bbdf2033238
Tran Trong Hien <tronghientran18@gmail.com>
2024-06-13 16:05:50 +0700

    what??

--- lib/clerk-convex-provider.tsx
+++ lib/clerk-convex-provider.tsx
@@ -4,7 +4,7 @@ import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(`${process.env.NEXT_PUBLIC_CONVEX_URL}`);
 
 const ClerkAuthenticationProvider = ({
   children,
